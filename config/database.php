4857309cb7a54b23becbb3d679396b55faf429ce
tapsobaalassane240-tech <tapsobaalassane240@gmail.com>
2026-03-21 15:59:29 +0000

    Update database.php

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "gestion_taches";
+$user = "root";
+$pass = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
