ec7c820b64b9f7d8ca85adcdc30fdb110afff6b2
tapsobaalassane240-tech <tapsobaalassane240@gmail.com>
2026-03-25 10:56:43 +0000

    Update dashboard.php

--- page/dashboard.php
+++ page/dashboard.php
@@ -1,13 +1,1 @@
-<?php
-include "../includes/auth.php";
-include "../config/database.php";
-?>
 
-<h2>Tableau de bord</h2>
-
-<a href="../logout.php">Se déconnecter</a>
-<br><br>
-
-<a href="taches/index.php">Gérer les tâches</a>
-<br>
-<a href="logs.php">Voir les logs</a>
